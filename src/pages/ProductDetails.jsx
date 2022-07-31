064d6d5d4e128f571caa1e50f85039087a7638e4
WILMER <wcaceres84@unab.edu.co>
2022-07-30 20:07:33 -0500

    Details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,13 +3,22 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux/es/exports';
 
 const ProductDetails = () => {
-    const {id}=useParams()
-    let products=useSelector(state=>state.productsSlice)
-    console.log(products)
-    
+
+    const { id } = useParams()
+    let product = useSelector(state => state.productsSlice)
+        .filter((e) => {
+            return parseInt(e.id) === parseInt(id)
+        })
+
+//Variable a usar "productDetail"  FABIAN
+
+    let productDetail = product[0]
+    console.log(productDetail)
+
     return (
         <div>
-            <span>{id}</span>
+            <span>{productDetail.title}</span>
+            <img src={productDetail.productImgs?.[0]} alt="" />
         </div>
     );
 };
