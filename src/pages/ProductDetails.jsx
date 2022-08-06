474f5e8cee7967fedd5e8805a83fec19c3f21d8c
FabianCasti <fcastillop@unicesar.edu.co>
2022-08-06 11:00:37 -0500

    anexos

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux/es/exports';
-// import { fabfacebook } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const ProductDetails = () => {
+
+    const [indexPhoto, setIndexPhoto] = useState(0)
 
     const { id } = useParams()
     let product = useSelector(state => state.productsSlice)
         .filter((e) => {
             return parseInt(e.id) === parseInt(id)
         })
-
-    //Variable a usar "productDetail"  FABIAN
-
-    // console.log(productDetail)
+   
     let allProducts = useSelector(state => state.productsSlice)
     let productDetail = product[0]
 
     let category = product[0].category.name
     let filtered = allProducts.filter((e) => {
         return e.category.name === category
-    })
-
-    console.log(filtered)
+    })    
 
     return (
 
@@ -37,13 +35,13 @@ const ProductDetails = () => {
 
                 <div className='Product-images'>
                     <div className='Product-Photo'>
-                        <img src={productDetail.productImgs?.[0]} alt="" />
+                        <img src={productDetail.productImgs?.[indexPhoto]} alt="" />
                     </div>
                     <div className='Product-gallery'>
                         <ul>
-                            <li > <img src={productDetail.productImgs?.[0]} alt="" /></li>
-                            <li className='selected'><img src={productDetail.productImgs?.[1]} alt="" /></li>
-                            <li > <img src={productDetail.productImgs?.[2]} alt="" /></li>
+                            <li onClick={()=>{setIndexPhoto(0)}}> <img src={productDetail.productImgs?.[0]} alt="" /></li>
+                            <li onClick={()=>{setIndexPhoto(1)}}> <img src={productDetail.productImgs?.[1]} alt="" /> </li>
+                            <li onClick={()=>{setIndexPhoto(2)}}> <img src={productDetail.productImgs?.[2]} alt="" /></li>
                         </ul>
                     </div>
 
@@ -82,9 +80,7 @@ const ProductDetails = () => {
                     </div>
 
                     <button className='add-cart-button'>
-                        Add to shop card
-                        <i className='icon-shopping-cart'>
-                        </i>
+                        Add to shop card <FontAwesomeIcon icon={faCartShopping} />
                     </button>
 
                 </div>
@@ -96,85 +92,105 @@ const ProductDetails = () => {
 
             <div className='Container-product-card-preview'>
 
-                <div className='Productcard0'>
-                    <div className='ImgprodcutPreview'>
-                        <img src={filtered[0].productImgs[0]} alt="" />
+
+                <div className='Productcard'>
+                    <div className='ImgproductPreview'>
+                        <img className='Primary' src={filtered[0].productImgs[0]} alt="" />
+                        <img className='Secundary' src={filtered[0].productImgs[1]} alt="" />
                     </div>
 
                     <div className='Inf-product-preview'>
                         <div className='ProductTittlePreview'>
                             <b>{filtered[0].title}</b>
                         </div>
-                        <div className='ProdcutPricePreview'>
+                        <div className='ProductPricePreview'>
                             <a>Price</a>
                             <div>
                                 <b> ${filtered[0].price} </b>
                             </div>
                         </div>
-                        <div className='ButtoCardPreview'>
-                            <button className='add-cartProductPreview'>
-                                Icon
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className='Productcard1'>
-                    <div className='ImgprodcutPreview'>
-                        <img src={filtered[1].productImgs[0]} alt="" />
+
                     </div>
 
-                    <div className='ProductTittlePreview'>
-                        <b>{filtered[1].title}</b>
-                    </div>
-                    <div className='ProdcutPricePreview'>
-                        <a>Price</a>
-                        <div>
-                            <b> ${filtered[1].price} </b>
-                        </div>
-                    </div>
                     <div className='ButtoCardPreview'>
                         <button className='add-cartProductPreview'>
-                            Icon
+                            <FontAwesomeIcon icon={faCartShopping} />
                         </button>
                     </div>
                 </div>
-                <div className='Productcard2'>
-                    <div className='ImgprodcutPreview'>
-                        <img src={filtered[2].productImgs[0]} alt="" />
+
+                <div className='Productcard'>
+                    <div className='ImgproductPreview'>
+                        <img className='Primary' src={filtered[1].productImgs[0]} alt="" />
+                        <img className='Secundary' src={filtered[1].productImgs[1]} alt="" />
                     </div>
 
-                    <div className='ProductTittlePreview'>
-                        <b>{filtered[2].title}</b>
-                    </div>
-                    <div className='ProdcutPricePreview'>
-                        <a>Price</a>
-                        <div>
-                            <b> ${filtered[2].price} </b>
+                    <div className='Inf-product-preview'>
+                        <div className='ProductTittlePreview'>
+                            <b>{filtered[1].title}</b>
                         </div>
+                        <div className='ProductPricePreview'>
+                            <a>Price</a>
+                            <div>
+                                <b> ${filtered[1].price} </b>
+                            </div>
+                        </div>
+
                     </div>
+
                     <div className='ButtoCardPreview'>
                         <button className='add-cartProductPreview'>
-                            Icon
+                            <FontAwesomeIcon icon={faCartShopping} />
+
                         </button>
                     </div>
                 </div>
-                <div className='Productcard3'>
-                    <div className='ImgprodcutPreview'>
-                        <img src={filtered[3].productImgs[0]} alt="" />
+                <div className='Productcard'>
+                    <div className='ImgproductPreview'>
+                        <img className='Primary' src={filtered[2].productImgs[0]} alt="" />
+                        <img className='Secundary' src={filtered[2].productImgs[1]} alt="" />
                     </div>
 
-                    <div className='ProductTittlePreview'>
-                        <b>{filtered[3].title}</b>
-                    </div>
-                    <div className='ProdcutPricePreview'>
-                        <a>Price</a>
-                        <div>
-                            <b> ${filtered[3].price} </b>
+                    <div className='Inf-product-preview'>
+                        <div className='ProductTittlePreview'>
+                            <b>{filtered[2].title}</b>
                         </div>
+                        <div className='ProductPricePreview'>
+                            <a>Price</a>
+                            <div>
+                                <b> ${filtered[2].price} </b>
+                            </div>
+                        </div>
+
                     </div>
                     <div className='ButtoCardPreview'>
                         <button className='add-cartProductPreview'>
-                            Icon
+                            <FontAwesomeIcon icon={faCartShopping} />
+                        </button>
+                    </div>
+                </div>
+                <div className='Productcard'>
+                    <div className='ImgproductPreview'>
+                        <img className='Primary' src={filtered[3].productImgs[0]} alt="" />
+                        <img className='Secundary' src={filtered[3].productImgs[1]} alt="" />
+                    </div>
+
+                    <div className='Inf-product-preview'>
+                        <div className='ProductTittlePreview'>
+                            <b>{filtered[3].title}</b>
+                        </div>
+                        <div className='ProductPricePreview'>
+                            <a>Price</a>
+                            <div>
+                                <b> ${filtered[3].price} </b>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='ButtoCardPreview'>
+                        <button className='add-cartProductPreview'>
+                            <FontAwesomeIcon icon={faCartShopping} />
                         </button>
                     </div>
                 </div>
@@ -185,7 +201,7 @@ const ProductDetails = () => {
                 <div class="copyright">© Academlo 2022</div>
                 <div class="social-networks">
                     <a href="https://www.instagram.com/academlohq/">
-                        <i class="icon-instagram"></i>
+                        <FontAwesomeIcon icon={faInstagram} />
                     </a>
                     <a href="https://www.linkedin.com/company/academlo/">
                         <i class="icon-linkedin"></i>
