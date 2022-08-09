4665f83a61c0b794f4da3d1bcfce2d49e1598fa9
WILMER <wcaceres84@unab.edu.co>
2022-08-08 23:23:23 -0500

    watch

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootswatch/dist/Litera/bootstrap.min.css";
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
