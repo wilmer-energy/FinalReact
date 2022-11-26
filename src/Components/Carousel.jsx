c7a43848d3bdcc52368f74b3b28618851bf6ca61
WILMER <wcaceres84@unab.edu.co>
2022-11-26 10:24:48 -0500

    created

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,47 +1,115 @@
-import React from 'react';
-import video from "../video/videocarrousel.mp4"
+import React from "react";
+import video from "../multimedia/videoplayback.mp4";
+import img1 from '../multimedia/Renovable.png'
+import cableado from '../multimedia/cableado_estructurado.jpg'
+import disenio from '../multimedia/disenio.jpg'
+import generador from '../multimedia/Generador.jpg'
+import mantenimiento from '../multimedia/mantenimiento-electrico-principal.png'
 
 const Carousel = () => {
-    return (
-        <>
-            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active" data-bs-interval="10000">
-                    {/* <img src="https://images.pexels.com/photos/4665064/pexels-photo-4665064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." /> */}
-                        <video src={video} autoPlay muted className="d-block w-100 videoc"></video>
-                        <div className="carousel-caption d-block">
-                            <h2>STATION INTELIGENT </h2>
-                          <a href="" className='btn btn-dark ps-4 pe-4'>Buy Now</a>
-                        </div>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img src="https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-block">
-                            <h2>SMARTPHONES Y SMART TV'S</h2>
-                            <a href="" className='btn btn-dark ps-4 pe-4'>Buy Now</a>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-block">
-                            <h2>COMPUTERS Y OTHERS </h2>
-                            <a href="" className='btn btn-dark ps-4 pe-4'>Buy Now</a>
-                        </div>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-
-
+  return (
+    <>
+      <div
+        id="carouselExampleInterval"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            {/* <img src="https://images.pexels.com/photos/4665064/pexels-photo-4665064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." /> */}
+            <video
+              src={video}
+              autoPlay
+              muted
+              className="d-block w-100 videoc"
+            ></video>
+            <div className="carousel-caption d-block">
+              <h2>Energias renovables</h2>
+              <a href="" className="btn btn-dark ps-4 pe-4">
+                Conoce más
+              </a>
             </div>
-        </>
-    );
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img
+              src={mantenimiento}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-block">
+              <h2>Mantenimiento eléctrico</h2>
+              <a href="" className="btn btn-dark ps-4 pe-4">
+                Conoce más
+              </a>
+            </div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img
+              src={cableado}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-block">
+              <h2>Cableado estructurado</h2>
+              <a href="" className="btn btn-dark ps-4 pe-4">
+              Conoce más
+              </a>
+            </div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img
+              src={disenio}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-block">
+              <h2>Diseño y contrucción</h2>
+              <a href="" className="btn btn-dark ps-4 pe-4">
+              Conoce más
+              </a>
+            </div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img
+              src={generador}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-block">
+              <h2>UPS y generadores</h2>
+              <a href="" className="btn btn-dark ps-4 pe-4">
+              Conoce más
+              </a>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Carousel;
--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
+import cableado from '../multimedia/cableado_estructurado.jpg'
+import disenio from '../multimedia/disenio.jpg'
+import generador from '../multimedia/Generador.jpg'
+import mantenimiento from '../multimedia/mantenimiento-electrico-principal.png'
+import img1 from '../multimedia/Renovable.png'
 
-const Category = ({ filter }) => {
+const Category = () => {
 
 
 
@@ -11,7 +16,7 @@ const Category = ({ filter }) => {
                 <div className='col-6 col-lg-3'>
                     <div className='border border-dark rounded text-center mt-3 mb-3 tl'>
                         <i className="fa-solid fa-credit-card pe-1 ps-1 pt-2 pb-2 pt-md-3 pb-md-3"></i>
-                        Pagos seguros
+                        Seguridad
                     </div>
 
                 </div>
@@ -21,7 +26,7 @@ const Category = ({ filter }) => {
 
                     <div className='border border-dark rounded text-center mt-3 mb-3 tl'>
                         <i className="fa-solid fa-truck pe-1 pt-2 pb-2 pt-md-3 pb-md-3"></i>
-                        Envio gratis</div>
+                        Eficientes</div>
 
                 </div>
 
@@ -30,51 +35,50 @@ const Category = ({ filter }) => {
 
                     <div className='border border-dark rounded text-center mt-3 mb-3 tl'>
                         <i className="fa-solid fa-lock pe-1 pt-2 pb-2 pt-md-3 pb-md-3"></i>
-                        100% segura</div>
+                        Expertos</div>
                 </div>
 
 
                 <div className='col-6 col-lg-3'>
                     <div className='border border-dark rounded text-center mt-3 mb-3 tl'>
                         <i className="fa-solid fa-tag pe-1 pt-2 pb-2 pt-md-3 pb-md-3"></i>
-                        10% OFF promo</div>
+                        Responsables</div>
 
                 </div>
 
             </div>
+            <h1>Mas de 7 años de experiencia en el mercado</h1>
+            <p>Servicios</p>
 
             <div className='row'>
                 <div className='col-12 col-sm-6'>
 
                     <div className='position-relative cur m-2' onClick={() => {
-                        filter(2)
                         window.scrollTo( 0, 1400 )
                     }}>
-                    <img className='img-fluid  im' src="https://images.pexels.com/photos/1092644/pexels-photo-1092644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>SMARTPHONE</h2>
+                    <img className='img-fluid  im' src={img1} alt="" />
+                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>Energias renovables</h2>
 
                 </div>
 
                 <div className='position-relative cur m-2' onClick={() => { 
-                    filter(1) 
                     window.scrollTo( 0, 1400 )}}>
-                    <img className='img-fluid im' src="https://images.pexels.com/photos/4006158/pexels-photo-4006158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>COMPUTERS</h2>
+                    <img className='img-fluid im' src={mantenimiento} alt="" />
+                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>Mantenimiento eléctrico</h2>
                 </div>
 
             </div>
 
             <div className='col-12 col-sm-6'>
-                <div className='position-relative cur m-2' onClick={() => { 
-                    filter(0) 
+                <div className='position-relative cur m-2' onClick={() => {  
                     window.scrollTo( 0, 1400 )}}>
-                    <img className='img-fluid im' src="https://images.pexels.com/photos/4009409/pexels-photo-4009409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>SMART TV</h2>
+                    <img className='img-fluid im' src={cableado} alt="" />
+                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>Cableado estructurado</h2>
                 </div>
 
                 <div className='position-relative cur m-2'>
-                    <img className='img-fluid im' src="https://images.pexels.com/photos/1841841/pexels-photo-1841841.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>OTHERS</h2>
+                    <img className='img-fluid im' src={disenio} alt="" />
+                    <h2 className='position-absolute top-50 start-50 translate-middle text-light sh'>Diseño y contrucción</h2>
                 </div>
 
             </div>
